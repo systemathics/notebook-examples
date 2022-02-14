4a6a96801afb8f9e624b21384a2e37c340a1ed4a
David Denis <david.denis@systemathics.com>
2022-02-14 11:57:19 +0100

    Added remoteaccess/jupyter-lab-windows.cmd

---
+++ remoteaccess/jupyter-lab-windows.cmd
@@ -0,0 +1,8 @@
+@echo off
+
+REM Use values from your dashboard token
+set CLIENT_ID=JgZp0I9dwmJYSLCUiQx9gNoZgcJZeMg4
+set CLIENT_SECRET=6IIIwHeajDiUgtzkwzZExA5Dnozq4WtCxU9EIeJHQXYrHkyRWddpIo_MDGDA3HKI
+ 
+REM Launches Jupyter lab from the python notebooks folder
+jupyter-lab.exe ..\python
