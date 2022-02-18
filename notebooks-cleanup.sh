2f26faa0a74996a4587993cbec0b13d748e21d68
David Denis <david.denis@systemathics.com>
2022-02-18 11:23:17 +0100

    added notebook-cleanups.sh

--- notebooks-cleanup.sh
+++ notebooks-cleanup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 ROOT_DIR="$(dirname "$(readlink -f "$0")")"
-jupyter nbconvert --ClearOutputPreprocessor.enabled=True --clear-output --inplace $ROOT_DIR/\*/\*/\*.ipynb
+
+pip install nbstripout
+find $ROOT_DIR -name "*.ipynb" -print0 | xargs -0 nbstripout \{};
