8dcc174df74e3ae1bb428f49714739b99f3eed86
David Denis <david.denis@systemathics.com>
2022-04-05 14:42:17 +0200

    tick apis v2 -> v1

--- remoteaccess/env.sh
+++ remoteaccess/env.sh
@@ -1,8 +1,17 @@
 # Fill the variables below
-export CLIENT_ID="<please-fill-in-here>"
-export CLIENT_SECRET="<please-fill-in-here>"
-#export TENANT="<please-fill-in-here-if-non-default>"
-#export AUDIENCE="<please-fill-in-here-if-non-default>"
-#export GRPC_APIS="<please-fill-in-here-if-non-default>"
+
+# remote (prod)
+#export CLIENT_ID="Of6cWBNuruUut9cv67yaoFgEGGmgQnEd"
+#export CLIENT_SECRET="xNolGV_-w-blBHRFX_h5KiLFtKBSHDy2Qv7MIGlaysakupMK2_wQZBF_8jEredqi"
+
+# remote (dev)
+#export CLIENT_ID="Of6cWBNuruUut9cv67yaoFgEGGmgQnEd"
+#export CLIENT_SECRET="xNolGV_-w-blBHRFX_h5KiLFtKBSHDy2Qv7MIGlaysakupMK2_wQZBF_8jEredqi"
+#export TENANT="ganymede-dev.eu.auth0.com"
+#export AUDIENCE="https://dev.ganymede-dev"
+#export GRPC_APIS="https://grpc.dev.systemathics.eu"
+
+# local
+export GRPC_APIS="http://172.29.32.1:5000" # windows
 
 env | egrep '(CLIENT_ID|CLIENT_SECRET|AUDIENCE|TENANT|GRPC_APIS)'
