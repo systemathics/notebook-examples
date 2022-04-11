6790f555afaad46f96db6cbad26557bb72c59518
Rémy Frèrebeau <remy.frerebeau@systemathics.com>
2022-04-11 11:04:17 +0200

    pre merge

--- notebooks-cleanup.sh
+++ notebooks-cleanup.sh
@@ -3,4 +3,7 @@
 ROOT_DIR="$(dirname "$(readlink -f "$0")")"
 
 pip install nbstripout
+find $ROOT_DIR -name "*.ipynb" -print0 | xargs -0 sed -i '/^ *"id": "[a-z0-9\-]*",$/d'
+find $ROOT_DIR -name "*.ipynb" -print0 | xargs -0 sed -i 's/nuget: Systemathics.Apis, 0.9.\*-pre\*/nuget: Systemathics.Apis/'
+find $ROOT_DIR -name "*.ipynb" -print0 | xargs -0 sed -i 's/pip install systemathics.apis --pre/pip install systemathics.apis/'
 find $ROOT_DIR -name "*.ipynb" -print0 | xargs -0 nbstripout \{};
